d1b2199557ca7864a24301c7d83f2cabe5f2e454
Daksh Sahni <dakshsahni2004@gmail.com>
2026-08-19 15:28:37 +0530

    Refactor vertex cover and add edge generation
    Refactored vertex cover algorithm and added edge generation functionality.

--- prac2.cpp
+++ prac2.cpp
@@ -1,93 +1,165 @@
 #include <bits/stdc++.h>
-#include <algorithm>
+#include <cstdlib>
+#include <ctime>
+
 using namespace std;
 
 void helper(int i, vector<int> curr, vector<vector<int>> &subsets, vector<int> vertices)
 {
-    if (i == vertices.size())
-    {
-        subsets.push_back(curr);
-        return;
-    }
+	if (i == vertices.size())
+	{
+		subsets.push_back(curr);
+		return;
+	}
 
-    curr.push_back(vertices[i]);
-    cout << vertices[i] << "\n";
-    helper(i + 1, curr, subsets, vertices);
-    curr.pop_back();
-    helper(i + 1, curr, subsets, vertices);
+	curr.push_back(vertices[i]);
+	// cout << vertices[i] << "\n";
+	helper(i + 1, curr, subsets, vertices);
+	curr.pop_back();
+	helper(i + 1, curr, subsets, vertices);
 }
 vector<vector<int>> genrateSubsets(int N)
 {
 
-    int i = 0;
-    vector<int> curr = {};
-    vector<vector<int>> subsets = {{}};
+	int i = 0;
+	vector<int> curr = {};
+	vector<vector<int>> subsets = {{}};
 
-    vector<int> vertices = {};
+	vector<int> vertices = {};
 
-    for (int i = 0; i < N; i++)
-    {
-        vertices.push_back(i);
-    }
-    helper(i, curr, subsets, vertices);
+	for (int i = 0; i < N; i++)
+	{
+		vertices.push_back(i);
+	}
+	helper(i, curr, subsets, vertices);
 
-    return subsets;
+	return subsets;
 }
 
 bool checkinsubset(int u, vector<int> subset)
 {
-    for (auto i : subset)
-        if (u == i)
-            return true;
+	for (auto i : subset)
+		if (u == i)
+			return true;
 
-    return false;
+	return false;
 }
 
 bool isVc(vector<int> subset, vector<pair<int, int>> edgeList)
 {
-    for (auto edge : edgeList)
-    {
-        if (!checkinsubset(edge.first, subset) && !checkinsubset(edge.second, subset))
-            return false;
-    }
-    return true;
+	for (auto edge : edgeList)
+	{
+		if (!checkinsubset(edge.first, subset) && !checkinsubset(edge.second, subset))
+			return false;
+	}
+	return true;
 }
 int vertexCover(vector<pair<int, int>> edgeList, int N, vector<int> &ans)
 {
 
-    vector<vector<int>> subsets;
-    int best = N + 1;
+	vector<vector<int>> subsets;
+	int best = N + 1;
 
-    subsets = genrateSubsets(N);
+	subsets = genrateSubsets(N);
 
-    for (int i = 0; i < subsets.size(); i++)
-    {
-        int subsetSize = subsets[i].size();
-        if (isVc(subsets[i], edgeList))
-        {
-            best = min(best, subsetSize);
-            ans = subsets[i];
+	for (int i = 0; i < subsets.size(); i++)
+	{
+		int subsetSize = subsets[i].size();
+		if (isVc(subsets[i], edgeList))
+		{
+			best = min(best, subsetSize);
+			ans = subsets[i];
+		}
+	}
+
+	return best;
+}
+
+vector<pair<int,int>> generateEdges(int n,int m) {
+	vector<pair<int,int>> edges;
+	set<pair<int,int>>edgesSet;
+
+	srand(time(0));
+
+
+	for(int i = 1;i<n;i++) {
+		int randomValue = rand() % i;
+			while(randomValue == i);
+
+			int first = min(i,randomValue);
+			int second = max(i,randomValue);
+			if(edgesSet.count({first,second})<= 0 ) {
+
+				edges.push_back({first,second});
+				edgesSet.insert({first,second});
+			}
+
+		}
+
+		while(edgesSet.size() != m) {
+		    
+			int r1 = rand()% n;
+			int r2 = rand()% n;
+
+			if(r1 != r2) {
+				int first = min(r1,r2);
+				int second = max(r2,r1);
+
+				if(edgesSet.count({first,second}) <=0) {
+					edgesSet.insert({first,second});
+					edges.push_back({first,second});
+				}
+			}
+
+		}
+
+		return edges;
+
+	}
+
+	int main()
+	{
+		vector<int> subsets;
+// 		int N = 7;
+
+		for(int m = 10 ;m<=45;m+=5){
+		    
+		vector<pair<int,int>> edges = generateEdges(10,m);
+		
+		string filename = "graph_m" + to_string(m) + ".txt";
+        ofstream outFile(filename);
+        
+            
+        for (const auto& edge : edges) {
+            outFile << edge.first << " " << edge.second << "\n";
+        
         }
-    }
+        outFile<<"vertex Cover"<<"\n";
+        
+        vector<int> subsets;
+		
+		vertexCover(edges,10,subsets);
+		
+	for (const auto& node:subsets){
+            outFile << node<<" ";
+        }
+        outFile<<"\n";
+		
+		
+        outFile.close();
+		
+		for(auto edge : edges) cout<<edge.first<<" "<<edge.second<<endl;
+		cout<<edges.size();cout<<endl;
+		
+		}
+		
+// 		vector<pair<int, int>> edgeList = {
+// 			{0, 1}, {0, 2}, {2, 1}, {1, 3}, {2, 3}, {2, 4}, {3, 4}, {3, 5}, {5, 6}, {6, 1}
+// 		};
+		// cout << vertexCover(edgeList, N, subsets);
+		// cout << endl;
 
-    return best;
-}
+		for (auto i : subsets)
+			cout << i;
 
-void datasetGenrator(int n = 10, int m = 45){//will maintain it as edge list
-    return ;
-}
-
-int main()
-{
-    
-    vector<int> ans;
-    int N = 7;
-    vector<pair<int, int>> edgeList = {
-        {0, 1}, {0, 2}, {2, 1}, {1, 3}, {2, 3}, {2, 4}, {3, 4}, {3, 5}, {5, 6}, {6, 1}};
-    cout << vertexCover(edgeList, N, ans);
-    cout << endl;
-    
-    for (auto i : ans)
-        cout << i;
-
-}
+	}
