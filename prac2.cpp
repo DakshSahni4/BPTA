9676f015bc8b3dd5acdb526f3ebb673d142b2fc1
DakshSahni4 <dakshsahni2004@gmail.com>
2026-09-11 00:34:18 +0530

    record duration

--- prac2.cpp
+++ prac2.cpp
@@ -1,10 +1,9 @@
 #include <bits/stdc++.h>
-
-
+#include <iomanip>
 
 using namespace std;
 
-vector<int> aproximation(vector<pair<int,int>>& edges,int N){
+vector<int> approximation(vector<pair<int,int>>& edges,int N,vector<pair<int,int>>& matching){
     unordered_set<int> visited;
     for( auto edge : edges){
         int u = edge.first;
@@ -12,6 +11,7 @@ vector<int> aproximation(vector<pair<int,int>>& edges,int N){
 
         if(visited.count(u) || visited.count(v)) continue;
         else {
+            matching.push_back({u,v});
             visited.insert(u);
             visited.insert(v);
         }
@@ -20,6 +20,7 @@ vector<int> aproximation(vector<pair<int,int>>& edges,int N){
     for(auto vertex : visited) aproximateVc.push_back(vertex);
     return aproximateVc;
 }
+
 bool loadGraph(const string& filename, vector<pair<int,int>>& edges) {
 
     ifstream inFile(filename);
@@ -40,11 +41,31 @@ bool loadGraph(const string& filename, vector<pair<int,int>>& edges) {
 }
 int main(){
     int N = 10;
-
-    string filename = "graph_m" + to_string(10) + ".txt";
+    
+    string filename = "graph_m" + to_string(45) + ".txt";
     vector<pair<int,int>> edges;
     loadGraph(filename,edges);
+    
+    vector<pair<int,int>> matching;
+auto start = chrono::high_resolution_clock::now();
 
-    vector<int> aproximateVc = aproximation(edges,N);
-    for(auto i : aproximateVc) cout<<i<<" ";
+vector<int> approximateVc = approximation(edges, N, matching);
+
+auto end = chrono::high_resolution_clock::now();
+
+auto nanoDuration =
+    chrono::duration_cast<chrono::nanoseconds>(end - start);
+
+auto microDuration =
+    chrono::duration_cast<chrono::microseconds>(end - start);
+
+double microDouble =
+    chrono::duration<double, std::micro>(end - start).count();
+
+cout << "Nanoseconds: " << nanoDuration.count() << endl;
+cout << "Microseconds (cast): " << microDuration.count() << endl;
+cout << "Microseconds (double): " << microDouble << endl;
+
+    for(auto edge : matching) cout<<edge.first<<" "<<edge.second<<endl;
+    for(auto i : approximateVc) cout<<i<<" ";
 }
